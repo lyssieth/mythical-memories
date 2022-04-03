8d712cf4a800482ccbab0aa6988fdcf11dbd5db3
Lyssieth <lys@lys.ee>
2022-04-03 18:32:01 +0300

    did some shit

---
+++ src/main/java/ee/lys/mythicalmemories/ItemInit.java
@@ -0,0 +1,10 @@
+package ee.lys.mythicalmemories;
+
+import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
+import net.minecraft.item.Item;
+import net.minecraft.util.Rarity;
+
+public class ItemInit implements ItemRegistryContainer {
+
+    public static final Item MEMORY = new Item(new Item.Settings().fireproof().maxCount(64).rarity(Rarity.EPIC));
+}
