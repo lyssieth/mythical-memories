eb9ed344edc1e03daefec44bafad2d5350c8b06f
Lyssieth <lys@lys.ee>
2022-04-04 15:54:40 +0300

    Did some things.

--- src/main/java/ee/lys/mythicalmemories/ItemInit.java
+++ src/main/java/ee/lys/mythicalmemories/ItemInit.java
@@ -6,5 +6,5 @@ import net.minecraft.util.Rarity;
 
 public class ItemInit implements ItemRegistryContainer {
 
-    public static final Item MEMORY = new Item(new Item.Settings().fireproof().maxCount(64).rarity(Rarity.EPIC));
+    public static final Item MEMORY = new Item(new Item.Settings().fireproof().maxCount(64).rarity(Rarity.EPIC).group(MythicalMemories.MOD_GROUP));
 }
--- src/main/java/ee/lys/mythicalmemories/MythicalMemories.java
+++ src/main/java/ee/lys/mythicalmemories/MythicalMemories.java
@@ -2,15 +2,20 @@ package ee.lys.mythicalmemories;
 
 import io.wispforest.owo.registration.reflect.FieldRegistrationHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MythicalMemories implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final String MOD_ID = "mythicalmemories";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static final ItemGroup MOD_GROUP = FabricItemGroupBuilder.create(new Identifier(MOD_ID, "group"))
+            .icon(() -> new ItemStack(ItemInit.MEMORY))
+            .build();
 
     @Override
     public void onInitialize() {
