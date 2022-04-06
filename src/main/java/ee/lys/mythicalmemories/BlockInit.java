626f9bc50dcc01a199e673db3bf42c078c58b7ca
Lyssieth <lys@lys.ee>
2022-04-06 16:38:52 +0300

    A

--- src/main/java/ee/lys/mythicalmemories/BlockInit.java
+++ src/main/java/ee/lys/mythicalmemories/BlockInit.java
@@ -9,9 +9,20 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.sound.BlockSoundGroup;
 
-public class BlockInit implements BlockRegistryContainer {
+/**
+ * The block initialization class for Mythical Memories.
+ *
+ * @author Lyssieth
+ */
+public final class BlockInit implements BlockRegistryContainer {
 
-    public static final MemoryCore MEMORY_CORE = new MemoryCore(FabricBlockSettings.of(Material.GLASS).strength(5.0f).requiresTool().luminance(3).sounds(BlockSoundGroup.GLASS));
+    public static final MemoryCore MEMORY_CORE = new MemoryCore(
+            FabricBlockSettings
+                    .of(Material.GLASS)
+                    .strength(5.0f)
+                    .requiresTool()
+                    .luminance(3)
+                    .sounds(BlockSoundGroup.GLASS));
 
     @Override
     public BlockItem createBlockItem(Block block, String identifier) {
--- src/main/java/ee/lys/mythicalmemories/ItemInit.java
+++ src/main/java/ee/lys/mythicalmemories/ItemInit.java
@@ -4,7 +4,7 @@ import io.wispforest.owo.registration.reflect.ItemRegistryContainer;
 import net.minecraft.item.Item;
 import net.minecraft.util.Rarity;
 
-public class ItemInit implements ItemRegistryContainer {
+public final class ItemInit implements ItemRegistryContainer {
 
     public static final Item MEMORY = new Item(new Item.Settings().fireproof().maxCount(64).rarity(Rarity.EPIC).group(MythicalMemories.MOD_GROUP));
 }
