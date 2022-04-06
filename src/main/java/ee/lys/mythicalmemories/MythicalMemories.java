831244df45190eb23ca49b2e061e9c906d06834e
Lyssieth <lys@lys.ee>
2022-04-06 16:24:33 +0300

    Edit stuff

--- src/main/java/ee/lys/mythicalmemories/MythicalMemories.java
+++ src/main/java/ee/lys/mythicalmemories/MythicalMemories.java
@@ -9,7 +9,7 @@ import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MythicalMemories implements ModInitializer {
+public final class MythicalMemories implements ModInitializer {
     public static final String MOD_ID = "mythicalmemories";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
